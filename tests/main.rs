2a2e4ae6d5185d525e0d76f7b489f8788a272e53
splurf <evandotschwartzentruber@gmail.com>
2024-01-17 03:46:09 -0500

    refac

--- tests/main.rs
+++ tests/main.rs
@@ -1,6 +1,6 @@
 #[test]
 fn expand_tilde_test() {
-    let expected = simple_home_dir::home_dir().unwrap().join("foo");
-    let resulted = simple_expand_tilde::expand_tilde("~/foo").unwrap();
-    assert_eq!(resulted, expected)
+    let expected = simple_home_dir::home_dir().map(|p| p.join("foo"));
+    let resulted = simple_expand_tilde::expand_tilde("~/foo");
+    assert_eq!(expected, resulted)
 }
