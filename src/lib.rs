df6baeba99e67ecd1cd7a69534632bfb7f453553
splurf <evandotschwartzentruber@gmail.com>
2024-01-17 03:35:14 -0500

    minor optimization

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,15 @@
 pub fn expand_tilde(path: impl AsRef<std::path::Path>) -> Option<std::path::PathBuf> {
     let p = path.as_ref();
 
-    Some(if p.starts_with("~") {
-        let mut home = simple_home_dir::home_dir()?;
+    let expanded = if p.starts_with("~") {
+        let mut base = simple_home_dir::home_dir()?;
 
         if !p.ends_with("~") {
-            let mut cmpts = p.components();
-            cmpts.next()?;
-            home.extend(cmpts);
+            base.extend(p.components().skip(1));
         }
-        home
+        base
     } else {
         p.to_path_buf()
-    })
+    };
+    expanded.exists().then_some(expanded)
 }
